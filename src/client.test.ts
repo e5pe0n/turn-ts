5e85520b3378080130e7f17cafa946b802586c20
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-10-02 21:25:24 +0900

    feat: impl client

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,17 +1,52 @@
-import { describe, expect } from "vitest";
-import { Client, Response } from "./client.js";
+import { describe, expect, it } from "vitest";
+import { Client, type SuccessResponse } from "./client.js";
+import { createSocket } from "node:dgram";
+import { decodeStunMsg, encodeStunMsg } from "./msg.js";
+import { classRecord, methodRecord } from "./header.js";
+import { addrFamilyRecord, compReqAttrTypeRecord } from "./attr.js";
 
 describe("req", () => {
 	describe("udp", () => {
 		describe("Binding Request", () => {
 			it("sends a request then receives a success response", async () => {
+				const server = createSocket("udp4");
+				server.on("message", (msg, rinfo) => {
+					const { header, attrs } = decodeStunMsg(msg);
+					const res = encodeStunMsg({
+						header: {
+							cls: classRecord.successResponse,
+							method: methodRecord.binding,
+							trxId: header.trxId,
+						},
+						attrs: [
+							{
+								type: compReqAttrTypeRecord["XOR-MAPPED-ADDRESS"],
+								value: {
+									family: addrFamilyRecord.ipV4,
+									addr: Buffer.from([0xde, 0x3e, 0xf7, 0x46]),
+									port: 54321,
+								},
+							},
+						],
+					});
+					server.send(res, rinfo.port, rinfo.address, (err, bytes) => {
+						if (err) {
+							throw err;
+						}
+					});
+				});
+				server.bind(12345, "127.0.0.1");
 				const client = new Client({
 					address: "127.0.0.1",
-					port: 13345,
+					port: 12345,
 					protocol: "udp",
 				});
 				const res = await client.req("request", "binding");
-				expect(res).toEqual({ address: "222.62.247.70" } satisfies Response);
+				server.close();
+				expect(res).toEqual({
+					address: "222.62.247.70",
+					port: 54321,
+				} satisfies SuccessResponse);
 			});
 		});
 	});
--- src/client.ts
+++ src/client.ts
@@ -7,13 +7,16 @@ import {
 } from "./header.js";
 import { fBuf } from "./helpers.js";
 import { createSocket, type Socket } from "node:dgram";
+import { decodeStunMsg } from "./msg.js";
+import type { XorMappedAddressAttr } from "./attr.js";
 
 export type MessageClass = Extract<keyof typeof classRecord, "request">;
 export type MessageMethod = keyof typeof methodRecord;
 export type Protocol = "udp" | "tcp";
 
-export type Response = {
+export type SuccessResponse = {
 	address: string; // Reflexive Transport Address
+	port: number;
 };
 
 export type ClientConfig = {
@@ -22,11 +25,19 @@ export type ClientConfig = {
 	protocol: Protocol;
 };
 
-function decodeSuccessResponse(buf: Buffer): string {
-	if (buf.length !== 4) {
-		throw new Error("invalid success response;");
-	}
+function bufToAddrStr(buf: Buffer): string {
 	return Array.from(buf.values()).map(String).join(".");
+}
+
+function decodeSuccessResponse(buf: Buffer): SuccessResponse {
+	const msg = decodeStunMsg(buf);
+	const {
+		value: { port, addr },
+	} = msg.attrs[0]! as XorMappedAddressAttr;
+	return {
+		port,
+		address: bufToAddrStr(addr),
+	};
 }
 
 export class Client {
@@ -44,22 +55,28 @@ export class Client {
 		this.#sock.bind();
 	}
 
-	async req(cls: MessageClass, method: MessageMethod): Promise<Response> {
+	async req(
+		cls: MessageClass,
+		method: MessageMethod,
+	): Promise<SuccessResponse> {
 		const trxId = randomBytes(12);
 		this.#trxMap.set(fBuf(trxId), trxId);
 		const hBuf = encodeHeader({
 			cls: classRecord[cls],
 			method: methodRecord[method],
 			trxId: randomBytes(12),
-			length: 20,
+			length: 0,
 		});
 		return new Promise((resolve, reject) => {
 			this.#sock.on("message", (msg) => {
 				this.#sock.removeAllListeners("message");
-				resolve({ address: decodeSuccessResponse(msg) });
+				const res = decodeSuccessResponse(msg);
+				resolve(res);
 			});
-			this.#sock.send(hBuf, this.#port, this.#address, (err) => {
-				reject(err);
+			this.#sock.send(hBuf, this.#port, this.#address, (err, bytes) => {
+				if (err) {
+					reject(err);
+				}
 			});
 		});
 	}
