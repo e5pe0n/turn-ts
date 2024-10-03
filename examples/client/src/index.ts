9d79555716f679968d514460352a7968b673548a
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-10-03 19:54:33 +0900

    chore: add example client code

---
+++ examples/client/src/index.ts
@@ -0,0 +1,12 @@
+import { Client } from "stun-ts";
+
+{
+	const client = new Client({
+		address: "74.125.250.129", // stun.l.google.com
+		port: 19302,
+		protocol: "udp",
+	});
+	const res = await client.req("request", "binding");
+	console.log(res);
+	process.exit(0);
+}
