fd6ba6b58507919a8ba38e8dfcc6cec85fc0d506
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-10-05 15:45:35 +0900

    test: mv Binding Request test into udp

--- src/client.test.ts
+++ src/client.test.ts
@@ -47,49 +47,49 @@ describe("req", () => {
 				reason: "Unauthorized",
 			} satisfies ErrorResponse);
 		});
-	});
 
-	describe("Binding Request", () => {
-		it("sends a request then receives a success response", async () => {
-			const server = createSocket("udp4");
-			server.on("message", (msg, rinfo) => {
-				const { header, attrs } = decodeStunMsg(msg);
-				const res = encodeStunMsg({
-					header: {
-						cls: classRecord.successResponse,
-						method: methodRecord.binding,
-						trxId: header.trxId,
-					},
-					attrs: [
-						{
-							type: compReqAttrTypeRecord["XOR-MAPPED-ADDRESS"],
-							value: {
-								family: addrFamilyRecord.ipV4,
-								addr: Buffer.from([0xde, 0x3e, 0xf7, 0x46]),
-								port: 54321,
-							},
+		describe("Binding Request", () => {
+			it("sends a request then receives a success response", async () => {
+				const server = createSocket("udp4");
+				server.on("message", (msg, rinfo) => {
+					const { header, attrs } = decodeStunMsg(msg);
+					const res = encodeStunMsg({
+						header: {
+							cls: classRecord.successResponse,
+							method: methodRecord.binding,
+							trxId: header.trxId,
 						},
-					],
+						attrs: [
+							{
+								type: compReqAttrTypeRecord["XOR-MAPPED-ADDRESS"],
+								value: {
+									family: addrFamilyRecord.ipV4,
+									addr: Buffer.from([0xde, 0x3e, 0xf7, 0x46]),
+									port: 54321,
+								},
+							},
+						],
+					});
+					server.send(res, rinfo.port, rinfo.address, (err, bytes) => {
+						if (err) {
+							throw err;
+						}
+					});
 				});
-				server.send(res, rinfo.port, rinfo.address, (err, bytes) => {
-					if (err) {
-						throw err;
-					}
+				server.bind(12345, "127.0.0.1");
+				const client = new Client({
+					address: "127.0.0.1",
+					port: 12345,
+					protocol: "udp",
 				});
+				const res = await client.req("request", "binding");
+				server.close();
+				expect(res).toEqual({
+					success: true,
+					address: "222.62.247.70",
+					port: 54321,
+				} satisfies SuccessResponse);
 			});
-			server.bind(12345, "127.0.0.1");
-			const client = new Client({
-				address: "127.0.0.1",
-				port: 12345,
-				protocol: "udp",
-			});
-			const res = await client.req("request", "binding");
-			server.close();
-			expect(res).toEqual({
-				success: true,
-				address: "222.62.247.70",
-				port: 54321,
-			} satisfies SuccessResponse);
 		});
 	});
 });
